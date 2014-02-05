140bdc57312093aba7a11ac6c892469141d06f60
John Olmsted <johnaolmsted@gmail.com>
2014-02-05 08:22:08 -0800

    Adds validations for Event

--- source/ar-propagate-validations/app/models/event.rb
+++ source/ar-propagate-validations/app/models/event.rb
@@ -1,2 +1,18 @@
+class DateValidator < ActiveModel::Validator
+  def validate(record)
+    date = record.date
+    if date < Date.today
+      record.errors[:base] << "Date cannot be in the past."
+    elsif !Date.valid_date?(date.year, date.month, date.day)
+      record.errors[:base] << "Date must be valid."
+    end
+  end
+end
+
 class Event < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :organizer_name, presence: true
+  validates :email, format: { with: /.*@.*\..*/ }
+  validates :date, presence: true
 end
