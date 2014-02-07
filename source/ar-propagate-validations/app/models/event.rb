f76bc064777ce87b6d4d8e149fb93b2ad644c5fd
Ricardo Rubio <rickarubio@gmail.com>
2014-02-06 19:05:42 -0800

    Add validation to check for valid email

--- source/ar-propagate-validations/app/models/event.rb
+++ source/ar-propagate-validations/app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   validates :organizer_name, :organizer_email, 
     :title, :date , presence: true
   validates :title, uniqueness: true
+  validates :organizer_email, format: { with: /[^!#\$%^&*](\w|\d)+@.+\..+/, message: "invalid email address"}
 end
