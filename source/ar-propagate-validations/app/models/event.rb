00df0356324ee6d8566965ecbcc085f5cfce519e
K Y <kevin@kevinyeh.com>
2014-02-09 17:41:40 -0800

    Add unit test for event validations

--- source/ar-propagate-validations/app/models/event.rb
+++ source/ar-propagate-validations/app/models/event.rb
@@ -1,2 +1,19 @@
+require 'date' 
+
+class FutureDateValidator < ActiveModel::Validator
+  def validate(record)
+    if record.date && record.date < Date.today
+      record.errors[:base] << "Date must be today or later"
+    end
+  end
+end
+
 class Event < ActiveRecord::Base
+	validates :organizer_name, presence: true
+	validates_format_of :organizer_name, 
+		:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+	validates :title, presence: true, uniqueness: true
+	validates :date, presence: true
+	validates_with FutureDateValidator
+
 end
--- source/ar-propagate-validations/spec/event_model_spec.rb
+++ source/ar-propagate-validations/spec/event_model_spec.rb
@@ -1,8 +1,47 @@
 require 'spec_helper'
+require 'date'
 
 describe Event do
-	it { should respond_to :organizer_name }
-	it { should respond_to :organizer_email }
-	it { should respond_to :title }
-	it { should respond_to :date }
+	before :all do
+		@event = Event.create(organizer_name: 'henry', organizer_email: 'henry@hens.com', 
+												title: 'the original', date: Date.today)
+	end
+
+	after :all do
+		@event.destroy
+	end
+
+	specify { expect(subject).to respond_to :organizer_name }
+	specify { expect(subject).to respond_to :organizer_email }
+	specify { expect(subject).to respond_to :title }
+	specify { expect(subject).to respond_to :date }
+	specify { expect(subject).to validate_presence_of(:date) }
+	specify { expect(subject).to validate_presence_of(:title) }
+	specify { expect(subject).to validate_uniqueness_of(:title) }
+	specify { expect(subject).to validate_presence_of(:organizer_name) }
+
+	describe 'date validations' do
+		it 'will not save events with dates in past' do
+			event = Event.new(organizer_name: 'henry', organizer_email: 'henry@hens.com', 
+													title: 'the henfest', date: Date.today-1)
+			expect(event).not_to be_valid
+		end
+
+		it 'will not save an event with invalid date' do
+			bad_date_str = Date.today.to_s
+			bad_date_str[5] = '5'
+			event = Event.new(organizer_name: 'henry', organizer_email: 'henry@hens.com', 
+													title: 'the henfest', date: bad_date_str)
+			expect(event).not_to be_valid
+		end
+	end
+
+	describe 'email validation' do
+		it 'will not save with an invalid email' do
+			event = Event.new(organizer_name: 'henry', title: 'the henfest',
+													 date: Date.today)
+			event.organizer_email = 'pp'
+			expect(event).not_to be_valid
+		end
+	end
 end
