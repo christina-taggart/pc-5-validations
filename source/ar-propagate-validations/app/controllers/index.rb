d6dd4537a5bbfb8a4920ef707bbc08cd1d2d4096
Emily Foley <foleyemilym@gmail.com>
2014-02-11 11:43:53 -0800

    updated date type in erb forms, errors showing properly in the view, events saving when valid

--- source/ar-propagate-validations/app/controllers/index.rb
+++ source/ar-propagate-validations/app/controllers/index.rb
@@ -12,13 +12,13 @@ get '/events/new' do
   erb :create_event
 end
 
-post '/events/create' do
+post '/events' do
   @new_event = Event.new(title: params[:title], organizer_name: params[:organizer_name], organizer_email: params[:organizer_email], date: params[:date])
   if @new_event.valid?
     @new_event.save
     redirect "events/#{@new_event.id}/show"
   else
-    @errors = @new_event.errors
+    @errors = @new_event.errors.values
     erb :errors
   end
 end
--- source/ar-propagate-validations/app/models/event.rb
+++ source/ar-propagate-validations/app/models/event.rb
@@ -1,7 +1,10 @@
 class Event < ActiveRecord::Base
-  validates :title, :organizer_name, :organizer_email, :date, presence: true
-  validates :title, uniqueness: true
-  validates_format_of :organizer_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_presence_of :title, message: "You must include the title"
+  validates_presence_of :organizer_name, message: "You must include the organizer's name"
+  validates_presence_of :organizer_email, message: "You must include the organizer's email"
+  validates_presence_of :date, message: "You must include the date"
+  validates_uniqueness_of :title, message: "Title already taken."
+  validates_format_of :organizer_email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create, message: "Email is invalid"
   validate :date_cannot_be_in_the_past
 
     def date_cannot_be_in_the_past
